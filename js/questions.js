5354e64b525a50dc6c6777247f2ff4f8c872b013
imam3890 <77424801+imam3890@users.noreply.github.com>
2022-01-18 20:18:15 +0700

    first commit

---
+++ js/questions.js
@@ -0,0 +1,126 @@
+// creating an array and passing the number, questions, options, and answers
+let questions = [
+    {
+        numb: 1,
+        question: "1.gif",
+        answer: "12",
+        options: [
+            "21",
+            "13",
+            "12",
+            "22"
+        ]
+    },
+    {
+        numb: 2,
+        question: "2.gif",
+        answer: "5",
+        options: [
+            "2",
+            "8",
+            "6",
+            "5"
+        ]
+    },
+    {
+        numb: 3,
+        question: "3.gif",
+        answer: "Tidak Ada Angka",
+        options: [
+            "4",
+            "6",
+            "12",
+            "Tidak Ada Angka"
+        ]
+    },
+    {
+        numb: 4,
+        question: "4.gif",
+        answer: "42",
+        options: [
+            "42",
+            "24",
+            "48",
+            "Tidak Ada Angka"
+        ]
+    },
+    {
+        numb: 5,
+        question: "5.png",
+        answer: "Tidak Ada Angka",
+        options: [
+            "21",
+            "124",
+            "55",
+            "Tidak Ada Angka"
+        ]
+    },
+    {
+        numb: 6,
+        question: "test.jpg",
+        answer: "7, 8, 9, 12, 14, 15",
+        options: [
+            "1, 3, 9, 12, 14, 15",
+            "7, 8, 9, 13, 14, 15",
+            "7, 8, 9, 12, 14, 15",
+            "2, 8, 9, 13, 14, 15"
+        ]
+    },
+    {
+        numb: 7,
+        question: "test2.jpg",
+        answer: "Kupu-kupu, Kura-kura",
+        options: [
+            "Kupu-kupu, Kura-kura",
+            "Kupu-kupu, Rusa",
+            "Capung, Kura-kura",
+            "Kecoak, Gajah"
+        ]
+    },
+    {
+        numb: 8,
+        question: "test3.jpg",
+        answer: "7, 13, 16, 8, 12, 9",
+        options: [
+            "2, 18, 16, 8, 12, 9",
+            "7, 13, 16, 8, 12, 9",
+            "7, 13, 16, 8, 18, 8",
+            "1, 10, 16, 6, 13, 9"
+        ]
+    },
+    {
+        numb: 9,
+        question: "test4.jpg",
+        answer: "45, 8, 29, 56, 6, 25",
+        options: [
+            "45, 6, 20, 56, 6, 25",
+            "46, 8, 20, 50, 0, 25",
+            "45, 8, 29, 56, 6, 25",
+            "46, 8, 28, 58, 0, 25"
+        ]
+    },
+    {
+        numb: 10,
+        question: "test5.png",
+        answer: "12, 2, 42, 74, 6",
+        options: [
+            "12, 3, 43, 71, 8",
+            "12, 2, 42, 74, 6",
+            "12, 2, 48, 71, 6",
+            "14, 2, 12, 74, 8"
+        ]
+    },
+    // you can uncomment the below codes and make duplicate as more as you want to add question
+    // but remember you need to give the numb value serialize like 1,2,3,5,6,7,8,9.....
+    //   {
+    //   numb: 6,
+    //   question: "Your Question is Here",
+    //   answer: "Correct answer of the question is here",
+    //   options: [
+    //     "Option 1",
+    //     "option 2",
+    //     "option 3",
+    //     "option 4"
+    //   ]
+    // },
+];
